d1d4b773bd73de6a5e871b4857e1ad529e581bc9
Ronnie Bugia <ronnie11@live.ca>
2020-04-04 20:22:17 -0500

    commit build

---
+++ build/precache-manifest.4f7ea9562ace06a326ffc9d87baea89e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc6ecc8e00ebd5fd733873bdc974f0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7795b29782396327d88",
+    "url": "/static/css/main.4a93880b.chunk.css"
+  },
+  {
+    "revision": "0c608be959a69ce1b9fb",
+    "url": "/static/js/2.8fbaec95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fbaec95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7795b29782396327d88",
+    "url": "/static/js/main.5f05c9f1.chunk.js"
+  },
+  {
+    "revision": "6f48f6326399375a1908",
+    "url": "/static/js/runtime-main.0d48592e.js"
+  },
+  {
+    "revision": "5c947e45d4e3743ded9ce550580de538",
+    "url": "/static/media/acoutera-logo.5c947e45.png"
+  },
+  {
+    "revision": "a2449c105b47832ebf2e9c6f2a9c8a11",
+    "url": "/static/media/profile-pic.a2449c10.jpg"
+  }
+]);
